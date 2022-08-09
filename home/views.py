6a0748689e7f45de1c6dc5cd60431053ac5cabe4
webmaster.youbee <webmaster.youbee@gmail.com>
2022-08-09 13:20:53 +0000

    change index view

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
+import requests
 from django.shortcuts import HttpResponse
 
 
 def index(request):
-    return HttpResponse('FRACTAL-EXPLORER ALIVE!!')
+    r = requests.get('https://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
