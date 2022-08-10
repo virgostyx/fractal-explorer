259748480323f893b3ed9c196925d6222b96489b
webmaster.youbee <webmaster.youbee@gmail.com>
2022-08-10 11:14:23 +0000

    added debug flag

--- fractal_explorer/settings.py
+++ fractal_explorer/settings.py
@@ -36,8 +36,8 @@ else:
     ALLOWED_HOSTS = []
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU:                                           # For Heroku
-    DEBUG = True
+# if not IS_HEROKU:                                           # For Heroku
+DEBUG = True
 
 # Application definition
 
